a798da738a0527be6fd804c02500bf647740493b
kindunq <humanb2box@gmail.com>
2018-10-24 21:44:46 +0900

    fix: delete many file and start commit

--- dunqgram/users/models.py
+++ dunqgram/users/models.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+# from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
+    GENDER_CHOICES = (
+        ('mele', 'Mele'),
+        ('female', 'Female'),
+        ('not-specified', 'Not specified')
+    )
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(null=True, max_length=140)
+    gender = models.CharField(null=True, max_length=80, choices=GENDER_CHOICES)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
