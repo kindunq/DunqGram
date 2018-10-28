9bd93b1630005246d2eb8120a9dbad6892ec58ca
kindunq <humanb2box@gmail.com>
2018-10-28 22:23:23 +0900

    docs: add comment

--- dunqgram/users/models.py
+++ dunqgram/users/models.py
@@ -6,6 +6,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+
+    """ User Model """
+
     GENDER_CHOICES = (
         ('mele', 'Mele'),
         ('female', 'Female'),
