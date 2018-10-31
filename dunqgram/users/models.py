caf3f97b5c3ca7ce001f02d66ca783c520157c66
kindunq <humanb2box@gmail.com>
2018-10-31 21:26:29 +0900

    docs: Add followers and followings models

--- dunqgram/users/models.py
+++ dunqgram/users/models.py
@@ -21,6 +21,8 @@ class User(AbstractUser):
     bio = models.TextField(null=True)
     phone = models.CharField(null=True, max_length=140)
     gender = models.CharField(null=True, max_length=80, choices=GENDER_CHOICES)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
