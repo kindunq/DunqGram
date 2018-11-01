9241434e6bc8c364cb7b91adcbd7aec1402057ca
kindunq <humanb2box@gmail.com>
2018-11-01 23:11:22 +0900

    docs: Add search fields and filter in Image admin pannel

--- dunqgram/images/models.py
+++ dunqgram/images/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from dunqgram.users import models as user_models
 
 # Create your models here.
 
 
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,6 +15,7 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
     """ Image Model """
     file = models.ImageField()
@@ -20,15 +23,26 @@ class Image(TimeStampedModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
 
+
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
     """ Comment Model """
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
     image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
 
+    def __str__(self):
+        return self.message
 
+
+@python_2_unicode_compatible
 class Like(TimeStampedModel):
     """ Like Model """
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
     image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return 'User : {} - Image Caption : {}'.format(self.creator.username, self.image.caption)
--- dunqgram/users/models.py
+++ dunqgram/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 # from django.db.models import CharField
 from django.db import models
 from django.urls import reverse
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
