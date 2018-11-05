7a768d756136620216a0158c13f874568649d463
kindunq <humanb2box@gmail.com>
2018-11-05 22:14:02 +0900

    docs: Add Image view with serializer

--- dunqgram/images/views.py
+++ dunqgram/images/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models, serializers
+class ListAllImages(APIView):
 
-# Create your views here.
+    def get(self, request, format=None):
+
+        all_images = models.Image.objects.all()
+
+        serializer = serializers.ImageSerializer(all_images, many=True)
+        
+        return Response(data=serializer.data)
+
