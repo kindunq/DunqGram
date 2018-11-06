9fb2891dd19d7af8b5cabd554701ede4ca87d66f
kindunq <humanb2box@gmail.com>
2018-11-06 21:54:08 +0900

    docs: Add comment, like relate_name for images

--- dunqgram/images/models.py
+++ dunqgram/images/models.py
@@ -18,21 +18,25 @@ class TimeStampedModel(models.Model):
 @python_2_unicode_compatible
 class Image(TimeStampedModel):
     """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
-        return '{} - {}'.format(self.location, self.caption)
+        return "{} - {}".format(self.location, self.caption)
 
 
 @python_2_unicode_compatible
 class Comment(TimeStampedModel):
     """ Comment Model """
+
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(
+        Image, on_delete=models.PROTECT, null=True, related_name="comments"
+    )
 
     def __str__(self):
         return self.message
@@ -41,8 +45,13 @@ class Comment(TimeStampedModel):
 @python_2_unicode_compatible
 class Like(TimeStampedModel):
     """ Like Model """
+
     creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(
+        Image, on_delete=models.PROTECT, null=True, related_name="likes"
+    )
 
     def __str__(self):
-        return 'User : {} - Image Caption : {}'.format(self.creator.username, self.image.caption)
+        return "User : {} - Image Caption : {}".format(
+            self.creator.username, self.image.caption
+        )
