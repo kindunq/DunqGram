8e2843df9ec247a1d5d1ba4e03f63f2692069b0d
kindunq <humanb2box@gmail.com>
2018-11-08 16:34:42 +0900

    feat:Add feedSerializer for feed in creator

--- dunqgram/images/serializers.py
+++ dunqgram/images/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from . import models
-
+from dunqgram.users import models as user_models
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,11 +14,21 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class FeedUserSerializer(serializers.ModelSerializer):
+  
+    class Meta:
+        model = user_models.User
+        fields = (
+            'username', 'profile_image'
+            )
+
+
 class ImageSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True)
     likes = LikeSerializer(many=True)
+    creator = FeedUserSerializer(many=True)
 
     class Meta:
         model = models.Image
-        fields = ("id", "file", "location", "caption", "comments", "likes")
+        fields = ("id", "file", "location", "caption", "comments", "likes", "creator")
 
