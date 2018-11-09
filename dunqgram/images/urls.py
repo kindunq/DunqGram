3eaaa943f263fd06718f68042d18bca7b060892b
kindunq <humanb2box@gmail.com>
2018-11-09 16:36:11 +0900

    feat: Add image like url

--- dunqgram/images/urls.py
+++ dunqgram/images/urls.py
@@ -4,4 +4,14 @@ from . import views
 app_name = "Feed"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
+    path("<int:id>/like", view=views.LikeImage.as_view(), name="like_image")
 ]
+
+
+#/images/3/like/
+
+
+#0 create the url and the view
+#1 take the id from the url
+#2 we want to find an image with this id
+#3 we want to create a like for that image
