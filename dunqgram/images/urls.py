f10dbe65ac411655d29c7396dc0a1aee65eda85b
kindunq <humanb2box@gmail.com>
2018-11-09 18:14:23 +0900

    docs: Add comment url

--- dunqgram/images/urls.py
+++ dunqgram/images/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "Feed"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<int:id>/like", view=views.LikeImage.as_view(), name="like_image")
+    path("<int:id>/like", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:id>/comment", view=views.CommentOnImage.as_view(), name="comment_image")
 ]
 
 
