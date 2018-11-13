638a4c5800ce3c97ee33902f8846e48069412278
kindunq <humanb2box@gmail.com>
2018-11-13 22:06:33 +0900

    feat: Add followers url

--- dunqgram/users/urls.py
+++ dunqgram/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("<int:id>/follow/", view=views.FollowUser.as_view(), name="follow_users"),
     path("<int:id>/unfollow/", view=views.UnFollowUser.as_view(), name="unfollow_users"),
     path("<username>/", view=views.UserProfile.as_view(), name="user_profile"),
+    path("<username>/followers", view=views.UserFollowers.as_view(), name="user_followers"),
 ]
