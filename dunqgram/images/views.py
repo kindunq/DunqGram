c19eded622753f089275cc7a951d3b5411bdfe8e
kindunq <humanb2box@gmail.com>
2018-11-14 22:39:14 +0900

    feat: Add search views

--- dunqgram/images/views.py
+++ dunqgram/images/views.py
@@ -117,4 +117,15 @@ class Search(APIView):
     def get(self, request, format=None):
         hashtags = request.query_params.get('hashtags', None)
 
-        print(hashtags)
+        hashtags = hashtags.split(",")
+
+        if hashtags is not None:
+            images = models.Image.objects.filter(tags__name__in=hashtags).distinct()
+
+            serializer = serializers.CountImageSerializer(images, many=True)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        else:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
