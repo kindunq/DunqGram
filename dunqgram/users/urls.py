9c95b0f818d2d170f1fd7201b76bc249bf180ab5
kindunq <humanb2box@gmail.com>
2018-11-15 22:48:10 +0900

    feat: Add search user by username

--- dunqgram/users/urls.py
+++ dunqgram/users/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path("explore/", view=views.ExploreUsers.as_view(), name="explore_users"),
     path("<int:id>/follow/", view=views.FollowUser.as_view(), name="follow_users"),
     path("<int:id>/unfollow/", view=views.UnFollowUser.as_view(), name="unfollow_users"),
-    path("<username>/", view=views.UserProfile.as_view(), name="user_profile"),
     path("<username>/followers", view=views.UserFollowers.as_view(), name="user_followers"),
     path("<username>/following", view=views.UserFollowing.as_view(), name="user_following"),
+    path("search/", view=views.Search.as_view(), name="search_user"),
+    path("<username>/", view=views.UserProfile.as_view(), name="user_profile"),
 ]
