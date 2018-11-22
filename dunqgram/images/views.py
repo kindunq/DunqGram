0c9955bc386c5df9fb099c41baaf58f8f12f7f65
kindunq <humanb2box@gmail.com>
2018-11-22 17:36:00 +0900

    feat: create func for comment delete

--- dunqgram/images/views.py
+++ dunqgram/images/views.py
@@ -139,3 +139,18 @@ class Search(APIView):
         else:
 
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class ModerateComments(APIView):
+
+    def delete(self, request, id, comment_id, format=None):
+
+        user = request.user
+
+        try:
+            comment_to_delete = models.Comment.objects.get(id=comment_id, image__id=id, image__creator=user)
+            comment_to_delete.delete()
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
