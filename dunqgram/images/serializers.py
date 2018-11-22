6f8d75fc991337b8e97372343786e623bd5eb4a1
kindunq <humanb2box@gmail.com>
2018-11-22 18:06:12 +0900

    docs: Add fields in created

--- dunqgram/images/serializers.py
+++ dunqgram/images/serializers.py
@@ -60,4 +60,4 @@ class ImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Image
-        fields = ("id", "file", "location", "caption", "comments", "like_count", "creator", "tags")
+        fields = ("id", "file", "location", "caption", "comments", "like_count", "creator", "tags", 'created_at')
