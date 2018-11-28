df8878d8ad2a3a0ccc29b3ec7636f8f067d287df
kindunq <humanb2box@gmail.com>
2018-11-28 15:48:52 +0900

    feat: Add urls for auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,8 +67,12 @@ THIRD_PARTY_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'rest_framework',  # REST framework
+    'rest_framework.authtoken',
     'taggit',  # tags for the photos
-    'taggit_serializer',
+    'taggit_serializer', #tag serializer
+    'rest_auth', # rest auth
+    'rest_auth.registration', #enable resistration
+
 ]
 LOCAL_APPS = [
     'dunqgram.users.apps.UsersAppConfig',
@@ -96,10 +100,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = 'users.User'
-# https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = 'users:redirect'
-# https://docs.djangoproject.com/en/dev/ref/settings/#login-url
-LOGIN_URL = 'account_login'
+
 
 # PASSWORDS
 # ------------------------------------------------------------------------------
@@ -231,9 +232,9 @@ ACCOUNT_ALLOW_REGISTRATION = env.bool('DJANGO_ACCOUNT_ALLOW_REGISTRATION', True)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_AUTHENTICATION_METHOD = 'username'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = False
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'optional'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = 'dunqgram.users.adapters.AccountAdapter'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
@@ -251,7 +252,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ),
 }
+
+REST_USE_JWT = True
+ACCOUNT_LOGOUT_ON_GET = True
--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,8 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
-    path("api-token-auth/", obtain_jwt_token),
+    path("rest-auth/", include('rest_auth.urls')),
+    path("rest-auth/registration/", include('rest_auth.registration.urls')),
     #install postman
     
     path(settings.ADMIN_URL, admin.site.urls),
