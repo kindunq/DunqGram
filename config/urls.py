21988fd53deb7cb52908c3ac599680099a092fe0
kindunq <humanb2box@gmail.com>
2018-12-11 17:11:43 +0900

    docs: Add the reactappview as a url

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from rest_framework_jwt.views import obtain_jwt_token
+from dunqgram import views
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
@@ -27,6 +28,7 @@ urlpatterns = [
         include("dunqgram.notify.urls", namespace="notify"),
     ),
     path("accounts/", include("allauth.urls")),
+    path("", views.ReactAppView.as_view()),
     # Your stuff: custom urls includes go here
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
